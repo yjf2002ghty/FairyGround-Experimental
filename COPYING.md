0440cc3c5aec973ef8cb273359ebf9ae8e099123
yjf2002ghty <yjf2002ghty@outlook.com>
2023-07-03 20:35:34 +0800

    Copied all stuff from fork of ianfab's fairyground

---
+++ COPYING.md
@@ -0,0 +1,23 @@
+# fairyground Credits and Attribution
+
+fairyground is distributed under the [GNU General Public License 3.0](LICENSE).
+
+## Assets
+
+Asset | Source | Author | License | Notes
+--- | --- | --- | --- | ---
+public/assets/images/pieces/merida | The [lila](https://github.com/ornicar/lila/tree/master/public/piece/merida) repository, the [pychess-variants](https://github.com/gbtami/pychess-variants/tree/master/static/images/pieces/merida) repository, [ffish-test](https://github.com/thearst3rd/ffish-test) | Armando Hernandez Marroquin | [GPLv2+], see [lila's COPYING.md](https://github.com/ornicar/lila/blob/master/COPYING.md) |
+public/assets/images/pieces/cburnett | [wikimedia commons](https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces) | various authors, main author cburnett | [CC BY-SA 3.0] |
+public/assets/sound/thearst3rd/move.wav | [ffish-test](https://github.com/thearst3rd/ffish-test) | [Terry Hearst](https://github.com/thearst3rd) | [CC0] (Public Domain)
+public/assets/sound/thearst3rd/capture.wav | [ffish-test](https://github.com/thearst3rd/ffish-test) | [Terry Hearst](https://github.com/thearst3rd) | [CC0] (Public Domain)
+public/assets/sound/thearst3rd/check.wav | ["Notification 1_1"](https://freesound.org/people/Joao_Janz/sounds/478502/) on freesound.org | [Joao_Janz](https://freesound.org/people/Joao_Janz/) | [CC BY-NC 3.0]
+public/assets/sound/thearst3rd/terminal.wav | ["Chord Alert Notification"](https://freesound.org/people/graham_makes/sounds/457518/) on freesound.org | [graham_makes](https://freesound.org/people/graham_makes/) | [CC BY 3.0]
+public/assets/images/board and public/assets/chessground.css | [chessground-examples](https://github.com/ornicar/chessground-examples) and [pychess-variants](https://github.com/gbtami/pychess-variants) | Lichess contributors and pychess-variants contributors (???) | [GPLv3+] |
+
+[CC BY-NC-SA 4.0]: https://creativecommons.org/licenses/by-nc-sa/4.0/
+[CC0]: https://creativecommons.org/share-your-work/public-domain/cc0/
+[CC BY-NC 3.0]: https://creativecommons.org/licenses/by-nc/3.0/
+[CC BY-SA 3.0]: https://creativecommons.org/licenses/by-sa/3.0/
+[CC BY 3.0]: https://creativecommons.org/licenses/by/3.0/
+[GPLv2+]: https://www.gnu.org/licenses/gpl-2.0.txt
+[GPLv3+]: https://www.gnu.org/licenses/gpl-3.0.txt
---
+++ generate_css.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# generate SVGs
+for i in {1..12};do for j in {1..10}; do sed "s#0 0 1200 1000#0 $(( 1000 - 100*j )) $(( 100*i )) $(( 100*j ))#" public/assets/images/board/blue_base.svg > "public/assets/images/board/blue${i}x${j}.svg";done;done
+# generate background CSS
+for i in {1..12};do for j in {1..10}; do echo -e ".blue.board${i}x${j} cg-board {\n  background-image: url('images/board/blue${i}x${j}.svg');\n}";done;done > public/assets/theme-backgrounds.css
+
+# generate theme-letters CSS
+rm public/assets/theme-letters.css
+for i in "white" "black";do
+    for j in {a..z};do
+        if [[ $i = "white" ]]
+        then
+           letter=${j^}
+        else
+           letter=${j}
+        fi
+        echo -e ".letters .cg-wrap piece.${j}-piece.${i} {background-image: url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='80px' height='80px'><text font-size='30' x='50%' y='50%' fill='${i}' class='$i' text-anchor='middle' dominant-baseline='central'>${letter}</text></svg>\");}" >> public/assets/theme-letters.css
+        # promoted pieces
+        echo -e ".letters .cg-wrap piece.p${j}-piece.${i} {background-image: url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='80px' height='80px'><text font-size='30' x='50%' y='50%' fill='${i}' class='$i' text-anchor='middle' dominant-baseline='central'>+${letter}</text></svg>\");}" >> public/assets/theme-letters.css
+    done
+done
+
+# generate CSS
+rm public/assets/generated.css
+for i in {1..12};do
+    for j in {1..10};do
+        echo -e ".board${i}x${j} .cg-wrap {\n  width: $(( i > j ? 640 : 640 * i / j ))px;\n  height: $(( j > i ? 640 : 640 * j / i ))px;\n}" >> public/assets/generated.css
+        echo -e ".board${i}x${j}.pockets .cg-wrap {\n  width: $(( i > j ? 640 * i / ( i + 2 )  : 640 * i / j * i / ( i + 2 ) ))px;\n  height: $(( j > i ? 640 * i / ( i + 2 ) : 640 * j / ( i + 2 ) ))px;\n}" >> public/assets/generated.css
+        echo -e ".board${i}x${j} cg-helper {\n  width: $(echo "scale=5;100 / ${i}" | bc)%;\n  height: $(echo "scale=5;100 / ${j}" | bc)%;\n}" >> public/assets/generated.css
+        echo -e ".board${i}x${j} cg-board square {\n  width: $(echo "scale=5;100 / ${i}" | bc)%;\n  height: $(echo "scale=5;100 / ${j}" | bc)%;\n}" >> public/assets/generated.css
+        echo -e ".board${i}x${j} .cg-wrap piece {\n  width: $(echo "scale=5;100 / ${i}" | bc)%;\n  height: $(echo "scale=5;100 / ${j}" | bc)%;\n}" >> public/assets/generated.css
+    done
+done
